9854859e0bb53913d7c364076c147b8fd67452b8
Kishor V <developer.kishor@gmail.com>
2018-11-18 01:21:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-curl -Ls git.io/vddgY | bash
+if [ ! crew -V 1>/dev/null ]; then curl -Ls git.io/vddgY | bash; fi
 crew install python27 eudev
 pip install python-uinput
 cd ~
 curl -Ls https://raw.githubusercontent.com/kishorv06/ChromePad/master/GamePad.py > GamePad.py
-cp GamePad.py /usr/lib/bin/GamePad.py
-printf "#!/bin/bash\n sudo -s\n modprob uinput\n python GamePad.py\n" > /usr/lib/bin/ChromePad
-chmod a+x /usr/lib/bin/ChromePad
+cp GamePad.py /usr/local/bin/GamePad.py
+printf "#!/bin/bash\n sudo -s\n modprob uinput\n python GamePad.py\n" > /usr/local/bin/ChromePad
+chmod a+x /usr/local/bin/ChromePad
+echo "Installed ChromePad."
+echo "To run enter 'chromepad' in shell"
