ddbd9436e03032f3a86639e7c7e327b99b925215
Kishor V <developer.kishor@gmail.com>
2018-11-18 01:31:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ pip install python-uinput
 cd ~
 curl -Ls https://raw.githubusercontent.com/kishorv06/ChromePad/master/GamePad.py > GamePad.py
 sudo cp GamePad.py /usr/local/bin/GamePad.py
-sudo printf "#!/bin/bash\n sudo -s\n modprob uinput\n python GamePad.py\n" > /usr/local/bin/ChromePad
+sudo cat <<EOT >> /usr/local/bin/ChromePad 
+#!/bin/bash
+sudo -s
+modprob uinput
+python GamePad.py
+EOT
 sudo chmod a+x /usr/local/bin/ChromePad
 echo "Installed ChromePad."
-echo "To run enter \'chromepad\' in shell"
+echo "To run enter chromepad in shell"
