f9a9c0d4f079de66d977aa36d016943e8cc79aac
Kishor V <developer.kishor@gmail.com>
2018-11-18 10:50:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ pip install python-uinput
 cd ~
 sudo rm /usr/local/bin/chromepad /usr/local/bin/GamePad.py 2>/dev/null
 curl -Ls https://raw.githubusercontent.com/kishorv06/ChromePad/master/GamePad.py > GamePad.py
-sudo cp GamePad.py /usr/local/bin/GamePad.py
-rm GamePad.py
+cp GamePad.py /usr/local/bin/GamePad.py
 sudo cat <<EOT >> /usr/local/bin/chromepad 
 #!/bin/bash
 if ! [[ \$EUID = 0 ]]; then
